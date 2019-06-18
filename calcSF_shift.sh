cb1b1953eeaae48e38ab90c6e909632c99868b7e
cmantill <cristina.ana.mantilla.suarez@cern.ch>
2019-06-18 21:27:18 +0200

    playing wth shift

--- calcSF_shift.sh
+++ calcSF_shift.sh
@@ -2,16 +2,13 @@
 object=$1
 algo=$2
 wp=$3
-whichbit=$4
-#For bit version, enter either 04 or 05
+whichbit=$4 #For bit version, enter either 04 or 05
 scale=$5
 scaledat=$6
 smear=$7 # by default should be 0.5
 smeardat=$8 # by default should be 0.1
 
 declare -a ptranges
-#ptranges=("low" "lowmed" "med")
-#ptranges=("low")
 ptranges=("incl")
 
 workdir=${object}"_"${algo}
@@ -23,10 +20,15 @@ do
     ##make templates
     echo "make templates"
     echo "using bit version ${whichbit}"
-    
+    rm -r -f T_n2/
+    rm -r -f T_n2_scale${scale}_${scaledat}_smear${smear}_${smeardat}/
+
     # for bacon skim -> comment this if the templates are already done in coffea
-    cmdpass=$(echo 'makeSFTemplates.C("'${object}'","'${algo}'","'${wp}'","'${ptrange}'","'${whichbit}'",true)')
-    cmdfail=$(echo 'makeSFTemplates.C("'${object}'","'${algo}'","'${wp}'","'${ptrange}'","'${whichbit}'",false)')
+    nbins=20
+    xmin=50
+    xmax=120
+    cmdpass=$(echo 'makeSFTemplates.C("'${object}'","'${algo}'","'${wp}'","'${ptrange}'","'${whichbit}'",true,'${nbins}','${xmin}','${xmax}')')
+    cmdfail=$(echo 'makeSFTemplates.C("'${object}'","'${algo}'","'${wp}'","'${ptrange}'","'${whichbit}'",false,'${nbins}','${xmin}','${xmax}')')
     root -l -q ${cmdpass}
     root -l -q ${cmdfail}
 
@@ -34,11 +36,11 @@ do
 
     ## add scale and smear
     inputname=${object}"_"${algo}"_"${wp}"_"${whichbit}"_"${ptrange}
-    python makeSmearShift.py --ifile ${workdir}/${inputname}_pass_pre.root --smear ${7} --scale ${5}
-    python makeSmearShift.py --ifile ${workdir}/${inputname}_fail_pre.root --smear ${7} --scale ${5}
+    #python makeSmearShift.py --ifile ${workdir}/${inputname}_pass_pre.root --smear ${7} --scale ${5}
+    #python makeSmearShift.py --ifile ${workdir}/${inputname}_fail_pre.root --smear ${7} --scale ${5}
 
-    #mv ${workdir}/${inputname}_pass_pre.root ${workdir}/${inputname}_pass.root
-    #mv ${workdir}/${inputname}_fail_pre.root ${workdir}/${inputname}_fail.root 
+    mv ${workdir}/${inputname}_pass_pre.root ${workdir}/${inputname}_pass.root
+    mv ${workdir}/${inputname}_fail_pre.root ${workdir}/${inputname}_fail.root 
 
     ## make datacard
     echo "make datacard"
@@ -63,15 +65,13 @@ do
     echo "Do the MultiDimFit"
     combine -M MultiDimFit -m 125 sf"_"${inputname}.root --algo=singles --robustFit=1 --cminDefaultMinimizerTolerance 5.
     echo "Run the FitDiagnostics"    
-    #combine -M FitDiagnostics -m 125 sf"_"${inputname}.root --saveShapes --saveWithUncertainties --robustFit=1 --cminDefaultMinimizerTolerance 5. -t -1 --setParameters scale=0.5
     combine -M FitDiagnostics -m 125 sf"_"${inputname}.root --saveShapes --saveWithUncertainties --robustFit=1 --cminDefaultMinimizerTolerance 5.
     mv fitDiagnostics.root sf"_"fitDiagnostics"_"${inputname}".root"
     mv sf.txt sf"_"datacard"_"${inputname}".txt"
     cd ../
-    # for the bacon templates
-    cmdmake=$(echo 'makePlots.C("'${workdir}'","'${inputname}'","'${wp}'","'${ptrange}'","'${whichbit}'",'${scale}','${scaledat}','${smear}','${smeardat}','50.','110.','20',"mass")') 
-    # for the coffea templates
-    #cmdmake=$(echo 'makePlots.C("'${workdir}'","'${inputname}'","'${wp}'","'${ptrange}'",'50.','110.','15',"mass")') 
+
+    # plots
+    cmdmake=$(echo 'makePlots.C("'${workdir}'","'${inputname}'","'${wp}'","'${ptrange}'","'${whichbit}'",'${scale}','${scaledat}','${smear}','${smeardat}','${xmin}','${xmax}.','${nbins}',"mass")') 
     root -l -q ${cmdmake}
 done
 
